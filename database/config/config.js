d6a14a774de07806a3f3d70a1093fb36d318c57d
JeshuaBenitez <jesuaeder.moshi2004@gmail.com>
2025-03-28 18:29:41 -0600

    Primer commit

---
+++ database/config/config.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+
+console.log("🔍 DEV_DATABASE_URL:", process.env.DEV_DATABASE_URL);
+
+module.exports = {
+  development: {
+    url: process.env.DEV_DATABASE_URL,
+    dialect: 'postgres'
+  },
+  test: {
+    url: process.env.TEST_DATABASE_URL,
+    dialect: 'postgres'
+  },
+  production: {
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres'
+  }
+};
---
+++ frontend/callrestapi.js
@@ -0,0 +1,51 @@
+// Archivo callrestapi.js
+var url = "http://localhost:3300/api/users"; // Asegúrate de que la URL sea correcta
+
+function postUser() {
+    console.log("POST user", url);
+    var myName = $('#name').val();
+    var myEmail = $('#email').val();
+    var myAge = $('#age').val();
+    var myComments = $('#comments').val();
+
+    var myUser = {
+        name: myName,
+        email: myEmail,
+        age: myAge,
+        comments: myComments
+    };
+
+    console.log(myUser);
+
+    $.ajax({
+        url: url,
+        type: 'POST',
+        dataType: 'json',
+        contentType: 'application/json',
+        success: function(data) {
+            console.log(data);
+            $('#resultado').html(JSON.stringify(data.user));
+        },
+        data: JSON.stringify(myUser)
+    });
+}
+
+function getUsers() {
+    console.log("GET usuarios", url);
+    $.getJSON(url, function(json) {
+        console.log(json);
+        var arrUsers = json.users;
+        var htmlTableUsers = '<table border="1">';
+        arrUsers.forEach(function(item) {
+            htmlTableUsers += '<tr>' +
+                '<td>' + item.id + '</td>' +
+                '<td>' + item.name + '</td>' +
+                '<td>' + item.email + '</td>' +
+                '<td>' + item.age + '</td>' +
+                '<td>' + item.comments + '</td>' +
+                '</tr>';
+        });
+        htmlTableUsers += '</table>';
+        $('#resultado').html(htmlTableUsers);
+    });
+}
